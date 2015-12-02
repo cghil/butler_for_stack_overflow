7e63e9eb5e538fa8bc91c208d5183097c397a0b9
cghil <cghilarducci@gmail.com>
2015-12-02 11:43:29 -0800

    added moment to bower added comments on singlequestion page added form for creating a new comment - not functional

--- app/scripts/controllers/singlequestion.js
+++ app/scripts/controllers/singlequestion.js
@@ -1,15 +1,34 @@
 'use strict'
 
 angular.module('myAuthApp')
-	.controller('QuestionCtrl', function(questionService, $scope, $log, $routeParams){
+	.controller('QuestionCtrl', function(questionService, $scope, $log, $routeParams, sessionService){
+		
+		$scope.userSession = sessionService.isAuthorized;
+		
+		$scope.showCommentForm = false;
+		
+		$scope.addCommentForm = function(){
+			$scope.showCommentForm = !$scope.showCommentForm;
+		};
+		
+		$scope.$watch(function(){
+			return sessionService.isAuthorized
+		}, function(){
+			$scope.userSession = sessionService.isAuthorized;
+			$scope.user = sessionService.currentUser();
+		})
+
 		var questionId = $routeParams.id;
 		var httpGetQuestion = questionService.getSingleQuestion(questionId);
 
+		$scope.moment = moment;
+
 		httpGetQuestion.then(function(response){
 			$scope.question = response.data.question
-			$scope.comments = response.data.comments
-			debugger
+			$scope.comments = response.data.comments.reverse();
+			// debugger
 		}, function(){
 
-		})
+		});
+
 	});
